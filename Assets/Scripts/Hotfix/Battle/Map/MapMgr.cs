079bf7eaa372efbcb331ce189543fb1b518c95bb
jiahao.wang <2206222540@qq.com>
2026-06-28 11:41:17 +0800

    提交mapmgr

--- Assets/Scripts/Hotfix/Battle/Map/MapMgr.cs
+++ Assets/Scripts/Hotfix/Battle/Map/MapMgr.cs
@@ -1,16 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class MapMgr : Singleton<MapMgr>
 {
-    public void CreateMap(int mapId)
+    private int _CurMapId = -1;
+
+    public override void Destroy()
     {
+        base.Destroy();
+    }
+    public override async UniTask AwakeAsync()
+    {
+        await base.AwakeAsync();
+    }
+
+
+    public bool EnterMap(int mapId)
+    {
+        if (_CurMapId > 0)
+            return false;
         var mapCfg = GameSchedule.Instance.GetMapCfg0(mapId);
+        if (mapCfg == null)
+            return false;
+        _CurMapId = mapId;
+        CreateMap(mapId);
+        return true;
     }
-    public void ClearMap(int maoId)
+    public void ExitMap()
+    {
+        ClearMap();
+        _CurMapId = -1;
+    }
+    private async void CreateMap(int mapId)
+    {
+    }
+
+    private void ClearMap()
     {
 
     }
 
+    public Vector3 GetCurMapStartPosition()
+    {
+        var mapCfg = GameSchedule.Instance.GetMapCfg0(_CurMapId);
+        var pos = new Vector3(mapCfg.fMapLbPos[0], mapCfg.fMapLbPos[1], mapCfg.fMapLbPos[2]);
+        return pos;
+    }
+    public Vector3 GetCurMapPlayerStartPosition()
+    {
+        var lbPos = GetCurMapStartPosition();
+        var mapCfg = GameSchedule.Instance.GetMapCfg0(_CurMapId);
+        var localPos = new Vector3(mapCfg.fPlayerLocalPos[0], mapCfg.fPlayerLocalPos[1], mapCfg.fPlayerLocalPos[2]);
+        return lbPos + localPos;
+
+    }
 }
